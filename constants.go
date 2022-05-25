abd2f11e27253628478768f1915425f0fdf824bf
raymond-design <raymondyan777@gmail.com>
2022-05-25 15:05:16 -0700

    directory restructuring

---
+++ constants.go
@@ -0,0 +1,4 @@
+package usgsgo
+
+// baseUrl is the base URL for the Ivs API.
+const baseUrl string = "https://waterservices.usgs.gov/nwis/iv/%s"
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package USGSgo is a lightweight client library for working with USGS Water APIs in Go.
+//
+// Reference: https://waterservices.usgs.gov/
+//
+// See README.md for more info.
+package usgsgo
